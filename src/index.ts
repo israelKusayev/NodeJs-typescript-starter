bce72b89a17b0628cca0b8faf9b71b90afc016b6
Israel Kusayev <israel.kusayev@gmail.com>
2019-10-11 10:55:56 +0300

    added dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import express from 'express';
 import logger from './utils/logger';
 import routes from './api/routes';
+import dotenv from 'dotenv';
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+
+if (envFound.error) {
+  // This error should crash whole process
+  throw new Error(`⚠️  Couldn't find .env file  ⚠️`);
+}
 
 const app = express();
 
